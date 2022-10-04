b5fb8fad3f726a4f28b252deaac5ef81be3fe4a6
ReynaEstrada <iscsoftware384@gmai.com>
2022-10-03 21:39:28 -0500

    changes_names

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Testimonio from './componentes/Testimonio';
 function App() {
   return (
     <div className='App'>
-      <div className='contenedor-principal'>
+      <div className='main-container'>
         <h1>Esto es lo que dicen nuestros alumnos sobre  freeCodeCamp:</h1>
         <Testimonio 
           nombre='Shawn Wang'
